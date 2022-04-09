4c2a8544f4f3aa14bc59cb9543176f58e79e8cfc
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-09 19:42:39 +0530

    JS File

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Require mongoose
-const mongoose = require ('mongoose') // like import 
+const mongoose = require ('mongoose'); // like import 
+const { toUnicode } = require('punycode');
 
 // Connecting with MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/StudentDB1",
@@ -10,3 +11,29 @@ mongoose.connect("mongodb://127.0.0.1:27017/StudentDB1",
 )
 .then(()=>console.log("Connection Successful..."))
 .catch(()=>console.log("Erroe, Not connecting..."))
+
+// Create Schema 
+const StuSchema = mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        course: String,
+        duration: Number,
+        registration: Boolean,
+        date: {type: Date, default: Date.now},
+    }
+);
+
+// Create Model -> By this we can create Collection 
+const Student = new mongoose.model("Student", StuSchema)
+
+// Create Document
+const data1 = new Student(
+{
+    name: "Sneha",
+    course: "B.tech",
+    duration: 4,
+    registration: true
+})
+data1.save()
+.then((doc)=>console.log("Document Created"))
+.catch((err)=>console.log(err));

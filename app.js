1e4cf4ce5d69c21f4a29bebf5e112e3368b08502
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-09 21:26:47 +0530

    JS File

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { toUnicode } = require('punycode');
 
 // Connecting with MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/StudentDB1",
+// these are warning 
 {   useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify: false
@@ -33,7 +34,49 @@ const data1 = new Student(
     course: "B.tech",
     duration: 4,
     registration: true
-})
+});
+const data2 = new Student(
+{
+    name: "Surbhi",
+    course: "B.tech",
+    duration: 4,
+    registration: true
+});
+const data3 = new Student(
+{
+    name: "Shikha",
+    course: "B.tech",
+    duration: 4,
+    registration: true
+});
+const data4 = new Student(
+{
+    name: "Richa",
+    course: "B.tech",
+    duration: 4,
+    registration: true
+});
+
+// CRUD Operations
+
 data1.save()
 .then((doc)=>console.log("Document Created"))
 .catch((err)=>console.log(err));
+
+Student.insertMany([data2, data3, data4])
+.then(doc=>console.log("All data inserted..."))
+.catch(err=>console.log("Error in insertion"));
+Student.find()
+.then(doc=>console.log("All data inserted..."))
+.then(doc=>console.log(doc))
+.catch(err=>console.log("Error in insertion"));
+
+
+Student.findByIDAndUpdate(ObjectID, {duration : 4})
+.then(doc=>console.log(doc))
+.catch(err=>console.log("Error in updation"));
+
+
+Student.findByIDAndDelete(ObjectID)
+.then(doc=>console.log(doc))
+.catch(err=>console.log("Error in deletion"));
